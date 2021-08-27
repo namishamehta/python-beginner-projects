0620b25b939ceed9b082a89a6f666dd68d2c5dd7
Namisha Mehta <89629062+namishamehta@users.noreply.github.com>
2021-08-27 13:05:40 +0530

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,13 @@
+"""
+Very Beginner Python Project by Namisha Mehta
+Madlibs using string concatenation
+"""
+adject = input("Enter an Adjective: ")
+verb01 = input("Enter a Verb: ")
+verb02 = input("VEnter a erb: ")
+famous_person = input("Enter the name of a Famous person: ")
+
+madlib = f"Computer programming is so {adject}! It makes me so excited all the time because \
+I love to {verb01}. Stay hydrated and {verb02} like you are {famous_person}!"
+
+print(madlib)
