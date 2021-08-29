2c6b46e0522b6e99e2f085d34e835ba6948f2b29
Namisha Mehta <89629062+namishamehta@users.noreply.github.com>
2021-08-29 11:41:57 +0530

    Add files via upload

---
+++ Guess_num_of_user.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 27 22:28:35 2021
+
+@author: namis
+"""
+
+import random
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'C':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f" {guess} Too low (L) or Too high(H)??")
+        if feedback == "L":
+            low = guess + 1
+        if feedback == "H":
+            high = guess - 1
+    print(f"Yayy, you guessed the num {guess} correctly")
+computer_guess(100)
+            
